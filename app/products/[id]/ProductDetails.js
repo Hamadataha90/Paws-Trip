6661b5f7bc659e6238f9a0700740951ccf68d775
hamada-taha <hstaha1190@gmail.com>
2025-04-23 14:31:46 +0200

    cart

--- app/products/[id]/ProductDetails.js
+++ app/products/[id]/ProductDetails.js
@@ -216,7 +216,7 @@ export default function ProductDetails({ product: initialProduct }) {
       const newItem = {
         id: selectedVariant.id,
         quantity: parseInt(quantity, 10),
-        title: selectedVariant.title,
+        title: product.title,
         price: parseFloat(adjustedPrice),
         image: mainImage,
         color,
